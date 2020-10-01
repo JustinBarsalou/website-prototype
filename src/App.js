29ce87092600c828a6992cdfd82ed03eaa170702
JustinBarsalou <justinbarsalou@gmail.com>
2020-10-01 17:01:34 -0500

    finished services section. font awesome icons are changed and are appropriate for their sub sections.

--- src/App.js
+++ src/App.js
@@ -49,33 +49,38 @@ function App() {
     <div className="row">
       <div className="col-lg-12 text-center">
         <h2 className="section-heading text-uppercase">Services</h2>
-        <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+        <h3 className="section-subheading text-muted">As an entry level web developer, I’m excited to start using my skills and provide assistance where it’s needed. Javascript and React have been my favorite tools to use, but I am always eager to learn more. I try to familiarize myself with common practices, and be able to adapt to things unfamiliar to me. I’ve created simple html pages, altered web applications, pulled information from REST API’S, and created my own basic web application with a login authentication. </h3>
+        <h3 className="section-subheading text-muted border-gone">Languages: HTML, CSS, JavaScript, Python, C++</h3>
+        <h3 className="section-subheading text-muted border-gone">Databases: MySQL</h3>
+        <h3 className="section-subheading text-muted border-gone">Libraries: React</h3>
+        <h3 className="section-subheading text-muted border-gone">Frameworks: Bootstrap</h3>
+        <h3 className="section-subheading text-muted border-small">Other: Node.js, Heroku, Firebase, Google Cloud</h3>
       </div>
     </div>
     <div className="row text-center">
       <div className="col-md-4">
         <span className="fa-stack fa-4x">
           <i className="fa fa-circle fa-stack-2x text-primary"></i>
-          <i className="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
-        </span>
-        <h4 className="service-heading">E-Commerce</h4>
-        <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-      </div>
-      <div className="col-md-4">
-        <span className="fa-stack fa-4x">
-          <i className="fa fa-circle fa-stack-2x text-primary"></i>
           <i className="fa fa-laptop fa-stack-1x fa-inverse"></i>
         </span>
-        <h4 className="service-heading">Responsive Design</h4>
-        <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+        <h4 className="service-heading">Web/Application Development</h4>
+        <p className="text-muted">With the knowledge I’ve gained from coding bootcamp, I have learned useful tools to create and update web applications. </p>
       </div>
       <div className="col-md-4">
         <span className="fa-stack fa-4x">
           <i className="fa fa-circle fa-stack-2x text-primary"></i>
-          <i className="fa fa-lock fa-stack-1x fa-inverse"></i>
+          <i className="fa fa-handshake-o fa-stack-1x fa-inverse"></i>
         </span>
-        <h4 className="service-heading">Web Security</h4>
-        <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+        <h4 className="service-heading">Client Experience</h4>
+        <p className="text-muted">Having worked in customer service, making sure the customer is happy has been my top priority. I do my best to ensure that customers are satisfied, and leave feeling better than they did before.</p>
+      </div>
+      <div className="col-md-4">
+        <span className="fa-stack fa-4x">
+          <i className="fa fa-circle fa-stack-2x text-primary"></i>
+          <i className="fa fa-users fa-stack-1x fa-inverse"></i>
+        </span>
+        <h4 className="service-heading">Teamwork</h4>
+        <p className="text-muted">I thrive best working with a team who have passion and drive towards the same goal. It’s important to rely on one another to improve and grow. </p>
       </div>
     </div>
   </div>
@@ -165,96 +170,6 @@ function App() {
   </div>
 </section>
 
-
-<section className="bg-light page-section" id="team">
-  <div className="container">
-    <div className="row">
-      <div className="col-lg-12 text-center">
-        <h2 className="section-heading text-uppercase">Our Amazing Team</h2>
-        <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col-sm-4">
-        <div className="team-member">
-          <img className="mx-auto rounded-circle" src="img/team/1.jpg" alt=""/>
-          <h4>Kay Garland</h4>
-          <p className="text-muted">Lead Designer</p>
-          <ul className="list-inline social-buttons">
-            <li className="list-inline-item">
-              <a href="#something">
-                <i className="fa fa-twitter"></i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="#something">
-                <i className="fa fa-facebook-f"></i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="#something">
-                <i className="fa fa-linkedin-in"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="col-sm-4">
-        <div className="team-member">
-          <img className="mx-auto rounded-circle" src="img/team/2.jpg" alt=""/>
-          <h4>Larry Parker</h4>
-          <p className="text-muted">Lead Marketer</p>
-          <ul className="list-inline social-buttons">
-            <li className="list-inline-item">
-              <a href="#something">
-                <i className="fa fa-twitter"></i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="#something">
-                <i className="fa fa-facebook-f"></i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="#something">
-                <i className="fa fa-linkedin-in"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="col-sm-4">
-        <div className="team-member">
-          <img className="mx-auto rounded-circle" src="img/team/3.jpg" alt=""/>
-          <h4>Diana Pertersen</h4>
-          <p className="text-muted">Lead Developer</p>
-          <ul className="list-inline social-buttons">
-            <li className="list-inline-item">
-              <a href="#something">
-                <i className="fa fa-twitter"></i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="#something">
-                <i className="fa fa-facebook-f"></i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="#something">
-                <i className="fa fa-linkedin-in"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col-lg-8 mx-auto text-center">
-        <p className="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
-      </div>
-    </div>
-  </div>
-</section>
 
 
 <section className="py-5">
