20fc0f01d51138394d9478d2a01038b8e979cb81
JustinBarsalou <justinbarsalou@gmail.com>
2020-10-01 14:50:06 -0500

    got base for my website. All font awesome and bootstrap template stuff are linked properly. Wroking from top to bottom, and have completed the top part with my intro, and nav bar capabilities.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './assets/css/bootstrap.css'
+import './assets/css/agency.css'
 
 ReactDOM.render(
   <React.StrictMode>
